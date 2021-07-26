abbe0d4f58c16e5c3218f046a38407b4f0806cd5
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2021-07-25 23:33:54 -0300

    feat: add controller Tutorials

---
+++ 3-mern-crud-simple/app/controllers/tutorials.controller.js
@@ -0,0 +1,37 @@
+const db = require('../models');
+const Tutorial = db.tutorials
+
+// create and save a new Tutorial
+exports.create = (req, res) => {
+
+}
+
+// retrieve all Tutorials from the databse
+exports.findAll = (req, res) => {
+
+}
+
+// find a single tutorials by the ID in the request
+exports.findOne = (req, res) => {
+
+}
+
+// update a tutorial by the id in the request
+exports.update = (req, res) => {
+
+}
+
+// delete a tutorial with the specified id in the request
+exports.delete = (req, res) => {
+
+}
+
+// delete all tutorials from the database
+exports.deleteAll = (req, res) => {
+
+}
+
+// find all published tutorials 
+exports.findAllPublished = (req, res) => {
+
+}
