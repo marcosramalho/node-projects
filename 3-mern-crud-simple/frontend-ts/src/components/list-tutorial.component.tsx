3fb839f748a7dacd27b3f0c362a3a0a5a16e50aa
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2021-07-28 00:53:38 -0300

    feat: add ListTutorial component

---
+++ 3-mern-crud-simple/frontend-ts/src/components/list-tutorial.component.tsx
@@ -0,0 +1,198 @@
+import React, { Component, ChangeEvent } from "react";
+
+import { Link } from 'react-router-dom';
+
+import TutorialService from "../services/tutorial.service";
+import ITutorial from "../types/ITutorial";
+
+
+type Props = {};
+
+type State = {
+  tutorials: Array<ITutorial>,
+  currentTutorial: ITutorial | null,
+  currentIndex: number,
+  searchTitle: string
+}
+
+export default class ListTutorials extends Component<Props, State> {
+
+  constructor(props: Props) {
+    super(props);
+
+    this.onChangeSearchTitle = this.onChangeSearchTitle.bind(this);
+    this.retrieveTutorials = this.retrieveTutorials.bind(this);
+    this.refreshList = this.refreshList.bind(this);
+    this.setActiveTutorial = this.setActiveTutorial.bind(this);
+    this.removeAllTutorials = this.removeAllTutorials.bind(this);
+    this.searchTitle = this.searchTitle.bind(this);
+
+    this.state = {
+      tutorials: [],
+      currentTutorial: null,
+      currentIndex: -1,
+      searchTitle: ""
+    };
+  }
+
+  componentDidMount() {
+    this.retrieveTutorials();
+  }
+
+  onChangeSearchTitle(e: ChangeEvent<HTMLInputElement>) {
+    const searchTitle = e.target.value;
+
+    this.setState({
+      searchTitle: searchTitle
+    });
+  }
+
+  retrieveTutorials() {
+    TutorialService.getAll()
+      .then(response => {
+        this.setState({
+          tutorials: response.data
+        });
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.error(e);
+      });
+  }
+
+  refreshList() {
+    this.retrieveTutorials();
+    this.setState({
+      currentTutorial: null,
+      currentIndex: -1
+    });
+  }
+
+  setActiveTutorial(tutorial: ITutorial, index: number) {
+    this.setState({
+      currentTutorial: tutorial,
+      currentIndex: index
+    });
+  }
+
+  removeAllTutorials() {
+    TutorialService.deleteAll()
+      .then(response => {
+        console.log(response.data);
+        this.refreshList();
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  searchTitle() {
+    this.setState({
+      currentTutorial: null,
+      currentIndex: -1
+    });
+
+    TutorialService.findByTitle(this.state.searchTitle)
+      .then(response => {
+        this.setState({
+          tutorials: response.data
+        });
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+  render() {
+    const { searchTitle, tutorials, currentTutorial, currentIndex } = this.state;
+
+    return (
+      <div className="list row">
+        <div className="col-md-8">
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search by title"
+              value={searchTitle}
+              onChange={this.onChangeSearchTitle}
+            />
+            <div className="input-group-append">
+              <button
+                className="btn btn-outline-secondary"
+                type="button"
+                onClick={this.searchTitle}
+              >
+                Search
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-6">
+          <h4>Tutorials List</h4>
+
+          <ul className="list-group">
+            {tutorials &&
+              tutorials.map((tutorial: ITutorial, index: number) => (
+                <li
+                  className={
+                    "list-group-item " +
+                    (index === currentIndex ? "active" : "")
+                  }
+                  onClick={() => this.setActiveTutorial(tutorial, index)}
+                  key={index}
+                >
+                  {tutorial.title}
+                </li>
+              ))}
+          </ul>
+
+          <button
+            className="m-3 btn btn-sm btn-danger"
+            onClick={this.removeAllTutorials}
+          >
+            Remove All
+          </button>
+        </div>
+        <div className="col-md-6">
+          {currentTutorial ? (
+            <div>
+              <h4>Tutorial</h4>
+              <div>
+                <label>
+                  <strong>Title:</strong>
+                </label>{" "}
+                {currentTutorial.title}
+              </div>
+              <div>
+                <label>
+                  <strong>Description:</strong>
+                </label>{" "}
+                {currentTutorial.description}
+              </div>
+              <div>
+                <label>
+                  <strong>Status:</strong>
+                </label>{" "}
+                {currentTutorial.published ? "Published" : "Pending"}
+              </div>
+
+              <Link
+                to={"/tutorials/" + currentTutorial.id}
+                className="badge bg-warning"
+              >
+                Edit
+              </Link>
+            </div>
+          ) : (
+            <div>
+              <br />
+              <p>Please click on a Tutorial...</p>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
