e3f66f037f2dbefbca1c290afc67a29908a230dd
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2021-07-28 00:02:31 -0300

    chore: add axios

--- 3-mern-crud-simple/frontend-ts/src/App.tsx
+++ 3-mern-crud-simple/frontend-ts/src/App.tsx
@@ -1,27 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import './App.css';
+import { Link, Route, Switch } from 'react-router-dom';
+import AddTutorial from './components/add-tutorial.component';
+import ListTutorials from './components/list-tutorial.component';
+import Tutorial from './components/tutorial.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <Link to="/tutorials" className="navbar-brand">Tutorials</Link>
+          <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to="/tutorials" className="nav-link">Tutorials</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/add" className="nav-link">Add</Link>
+          </li>                    
+          
+        </div>
+        </nav>        
+        <div className="container mt-3">
+          <Switch>
+            <Route exact path={["/", "/tutorials"]} component={ListTutorials} />
+            <Route exact path="/add" component={AddTutorial} />
+            <Route path="/tutorials/:id" component={Tutorial} />
+          </Switch>
+        </div>
+
+      </div>
+    )    
+  }
 }
 
 export default App;
