a6d452f6bf01bba39b7eb68813f7c1d1ce322988
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2021-08-04 00:05:47 -0300

    feat: add component AddTutorial

--- 3-mern-crud-simple/frontend-hook/src/App.js
+++ 3-mern-crud-simple/frontend-hook/src/App.js
@@ -1,11 +1,42 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
 
+import AddTutorialsComponent from "./components/AddTutorialsComponent";
+import TutorialsComponent from "./components/TutorialsComponent";
+import ListTutorialsComponent from "./components/ListTutorialsComponent";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
 function App() {
   return (
-    <h1>OK</h1>
+    <div>
+      <nav className="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/tutorials" className="navbar-brand">
+          bezKoder
+        </a>
+        <div className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to={"/tutorials"} className="nav-link">
+              Tutorials
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to={"/add"} className="nav-link">
+              Add
+            </Link>
+          </li>
+        </div>
+      </nav>
 
+      <div className="container mt-3">
+        <Switch>
+          <Route exact path={["/", "/tutorials"]} component={ListTutorialsComponent} />
+          <Route exact path="/add" component={AddTutorialsComponent} />
+          <Route path="/tutorials/:id" component={TutorialsComponent} />
+        </Switch>
+      </div>
+    </div>
   );
 }
 
