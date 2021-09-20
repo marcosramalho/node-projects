c0514a3b354bf5425aa69ea76851cd1018f09429
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2021-09-20 17:45:11 -0300

    feat: add injection service Course in CourseController

--- 6-nest-training/src/courses/courses.controller.ts
+++ 6-nest-training/src/courses/courses.controller.ts
@@ -3,13 +3,10 @@ import {
   Controller,
   Delete,
   Get,
-  HttpCode,
-  HttpStatus,
   Param,
   Patch,
   Post,
   Put,
-  Res,
 } from '@nestjs/common';
 import { CoursesService } from './courses.service';
 
@@ -18,33 +15,32 @@ export class CoursesController {
   constructor(private coursesService: CoursesService) {}
 
   @Get()
-  findAll(@Res() response) {
-    return response.status(200).send('Lista de cursos');
+  findAll() {
+    return this.coursesService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: number) {
-    return `Curso #${id}`;
+    return this.coursesService.findOne(id);
   }
 
   @Post()
-  @HttpCode(HttpStatus.NO_CONTENT)
   create(@Body() body) {
-    return body;
+    return this.coursesService.create(body);
   }
 
   @Patch(':id')
   updatePartial(@Param('id') id: number, @Body() body) {
-    return `Updating partial course #${id}`;
+    return this.coursesService.update(id, body);
   }
 
   @Put(':id')
   update(@Param('id') id: number, @Body() body) {
-    return `updating course #${id}`;
+    return this.coursesService.update(id, body);
   }
 
   @Delete(':id')
   delete(@Param('id') id: number) {
-    return `deleting course #${id}`;
+    return this.coursesService.remove(id);
   }
 }
