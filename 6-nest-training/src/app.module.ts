9f0336d9515e0acf23caa4368dd2ac5138edfab3
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2021-09-20 23:10:44 -0300

    feat: add typeorm and connection database

--- 6-nest-training/src/app.module.ts
+++ 6-nest-training/src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoursesModule } from './courses/courses.module';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [CoursesModule],
+  imports: [
+    ConfigModule.forRoot(),
+    CoursesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
