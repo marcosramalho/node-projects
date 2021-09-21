8d95ed573c0230e3dbf051a7e305e9317cb05aa0
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2021-09-20 23:31:50 -0300

    feat: add Entity typeorm

--- 6-nest-training/src/courses/entities/course.entity.ts
+++ 6-nest-training/src/courses/entities/course.entity.ts
@@ -1,7 +1,20 @@
+import { type } from 'os';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Course {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   description: string;
+
+  @Column()
   price: number;
+
+  @Column('json', { nullable: true })
   tags: string[];
 }
